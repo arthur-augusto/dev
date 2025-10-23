7fe85146cc43426ffd60a378b8b7c97ef0d14f20
arthur-augusto <arthur.augusto.rp@gmail.com>
2025-10-22 22:01:19 -0300

    Mudando o tema do neovim

--- env/.config/nvim/lua/plugins/lualine.lua
+++ env/.config/nvim/lua/plugins/lualine.lua
@@ -2,13 +2,7 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require("lualine").setup({
-            options = {
-                icons_enabled = false,
-                theme = 'gruvbox_dark',
-                component_separators = '',
-                section_separators = '',
-            }
-        })
+        vim.cmd('colorscheme github_dark_high_contrast')
+        require('lualine').setup({})
     end
 }
